06ff5480312057d0e9b6d7b038df86e163db888b
Abdülbaki Kaplan <abkaplan@partner.innova.com.tr>
2021-01-23 14:20:48 +0300

    first commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Abdülbaki Kaplan on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
